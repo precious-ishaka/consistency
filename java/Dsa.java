eaf355018bcb804c6d5448d87131bdfc9ea80986
Precious <preciousitachi56@gmail.com>
2026-05-13 23:02:58 -0700

    Linked List

--- java/Dsa.java
+++ java/Dsa.java
@@ -19,18 +19,24 @@ public static void main(String[] args){
    }
 
 
-   for( int n : nums){
-      System.out.print(n + " ");
-   }
+   // for( int n : nums){
+   //    System.out.print(n + " ");
+   // }
 
 
 
 
 
 LinkedL listt = new LinkedL();
-listt.insert(4);
+listt.insert(14);
 listt.insert(6);
 listt.insert(17);
+listt.insert(55);
+listt.insertStart(4);
+listt.insertStart(-5);
+listt.insert(-7);
+listt.insertAt(2, 8);
+listt.show();
 
 
 }
@@ -43,31 +49,60 @@ class Node{
    Node next;
 }
 
-class LinkedL{
+
+  class LinkedL{
+
    Node head;
-   public void insert(int d ){
-      Node node = new Node();
-        node.data = d;
-      if(head == null){
-       w
-         head = node;
-      }else{
-          Node n  = head;
-          while(n.next != null){
-            n = n.next;
-          }
-
-          n.next = node;
-      }
-   }
-
    public void show(){
-      Node node = head;
-      while(node.next != null){
-         System.out.print(node.data + " ");
-          node = node.next;
-      }
-      System.out.print(node.data + " ");
+     Node n = head;
+     while(n.next != null){
+      System.out.print(n.data + " ");
+      n = n.next;
+     }
+     System.out.print(n.data);
    }
+
+
+   public void insert(int x){
+   Node node = new Node();
+   node.data = x;
+   if(head == null){
+      head = node;
+   }else{
+      Node n = head;
+      while(n.next != null){
+         n = n.next;
+      }
+
+      n.next = node;
+   }}
+
+   public void insertStart(int x){
+        Node node = new Node();
+        node.data = x;
+        node.next = null;
+        node.next = head;
+        head = node;
+
+   }
+
+
+public void insertAt(int index, int x){
+   Node node = new Node();
+   node.data = x;
+   node.next = null;
+   if(index == 0){ insertStart(x);}
+
+   Node n = head;
+   for(int i = 0; i < index -1; i++){
+      n = n.next;
+   }
+   node.next = n.next;
+   n.next = node;
+
 
 }
+
+
+
+  }
